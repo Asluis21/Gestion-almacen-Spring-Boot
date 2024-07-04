6b852b3a05c07f3ab3b516e03dc4e489ed1ea1be
Asluis21 <luis_alvarado1225@outlook.com>
2024-07-04 14:37:38 -0500

    initializer comentado

--- src/main/java/com/inventario/rasa/initializer/DefaultUserInitializer.java
+++ src/main/java/com/inventario/rasa/initializer/DefaultUserInitializer.java
@@ -47,164 +47,164 @@ public class DefaultUserInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
         
         //CATEGORIA
-        Categoria categoria = new Categoria();
-        categoria.setNombre("solidos");
-        categoria.setEstado(true);
+        // Categoria categoria = new Categoria();
+        // categoria.setNombre("solidos");
+        // categoria.setEstado(true);
         
-        Categoria categoria2 = new Categoria();
-        categoria2.setNombre("liquido");
-        categoria2.setEstado(true);
+        // Categoria categoria2 = new Categoria();
+        // categoria2.setNombre("liquido");
+        // categoria2.setEstado(true);
         
-        Categoria categoria3 = new Categoria();
-        categoria3.setNombre("gases");
-        categoria3.setEstado(true);
+        // Categoria categoria3 = new Categoria();
+        // categoria3.setNombre("gases");
+        // categoria3.setEstado(true);
 
-        categoriaService.crearCategoria(categoria);
-        categoriaService.crearCategoria(categoria2);
-        categoriaService.crearCategoria(categoria3);
+        // categoriaService.crearCategoria(categoria);
+        // categoriaService.crearCategoria(categoria2);
+        // categoriaService.crearCategoria(categoria3);
 
-        categoriaService.eliminarCategoriaByIdLogico(3L);
+        // categoriaService.eliminarCategoriaByIdLogico(3L);
 
         //PROVEEDOR
-        Proveedor proveedor = new Proveedor();
-        proveedor.setNombre("LG");
-        proveedor.setRuc("20287310291");
-        proveedor.setEstado(true);
+        // Proveedor proveedor = new Proveedor();
+        // proveedor.setNombre("LG");
+        // proveedor.setRuc("20287310291");
+        // proveedor.setEstado(true);
         
-        Proveedor proveedor2 = new Proveedor();
-        proveedor2.setNombre("Adidas");
-        proveedor2.setRuc("20929210291");
-        proveedor2.setEstado(true);
+        // Proveedor proveedor2 = new Proveedor();
+        // proveedor2.setNombre("Adidas");
+        // proveedor2.setRuc("20929210291");
+        // proveedor2.setEstado(true);
 
-        Proveedor proveedor3 = new Proveedor();
-        proveedor3.setNombre("Sol");
-        proveedor3.setRuc("20289110911");
-        proveedor3.setEstado(true);
+        // Proveedor proveedor3 = new Proveedor();
+        // proveedor3.setNombre("Sol");
+        // proveedor3.setRuc("20289110911");
+        // proveedor3.setEstado(true);
 
 
-        proveedorService.crearProveedor(proveedor);
-        proveedorService.crearProveedor(proveedor2);
-        proveedorService.crearProveedor(proveedor3);
+        // proveedorService.crearProveedor(proveedor);
+        // proveedorService.crearProveedor(proveedor2);
+        // proveedorService.crearProveedor(proveedor3);
 
-        proveedorService.eliminarProveedorPorIdLogico(2L);
+        // proveedorService.eliminarProveedorPorIdLogico(2L);
 
         // ROLES 
-        Rol rolOperario = new Rol();
-        rolOperario.setNombre("OPERARIO");
-        rolOperario.setEstado(true);
+        // Rol rolOperario = new Rol();
+        // rolOperario.setNombre("OPERARIO");
+        // rolOperario.setEstado(true);
 
-        Rol rolJefe = new Rol();
-        rolJefe.setNombre("JEFE");
-        rolJefe.setEstado(true);
+        // Rol rolJefe = new Rol();
+        // rolJefe.setNombre("JEFE");
+        // rolJefe.setEstado(true);
 
-        Rol rolAuxiliar = new Rol();
-        rolAuxiliar.setNombre("AUXILIAR");
-        rolAuxiliar.setEstado(true);
+        // Rol rolAuxiliar = new Rol();
+        // rolAuxiliar.setNombre("AUXILIAR");
+        // rolAuxiliar.setEstado(true);
 
         
-        rolService.crearRol(rolJefe);
-        rolService.crearRol(rolOperario);
-        rolService.crearRol(rolAuxiliar);
+        // rolService.crearRol(rolJefe);
+        // rolService.crearRol(rolOperario);
+        // rolService.crearRol(rolAuxiliar);
 
-        rolService.eliminarRolByIdLogico(3L);
+        // rolService.eliminarRolByIdLogico(3L);
         
         // ALMACENES
-        Almacen almacen = new Almacen();
-        almacen.setNombre("Almacen principal");
-        almacen.setUbicacion("Av. Argentina #293");
-        almacen.setEstado(true);
+        // Almacen almacen = new Almacen();
+        // almacen.setNombre("Almacen principal");
+        // almacen.setUbicacion("Av. Argentina #293");
+        // almacen.setEstado(true);
         
-        Almacen almacen2 = new Almacen();
-        almacen2.setNombre("Almacen secundario");
-        almacen2.setUbicacion("Av. Peru #102");
-        almacen2.setEstado(true);
+        // Almacen almacen2 = new Almacen();
+        // almacen2.setNombre("Almacen secundario");
+        // almacen2.setUbicacion("Av. Peru #102");
+        // almacen2.setEstado(true);
 
-        almacenService.crearAlmacen(almacen);
-        almacenService.crearAlmacen(almacen2);
+        // almacenService.crearAlmacen(almacen);
+        // almacenService.crearAlmacen(almacen2);
         
-        almacenService.eliminarAlmacenByIdLogico(2L);
+        // almacenService.eliminarAlmacenByIdLogico(2L);
 
         // USUARIOS
-        UsuarioRequestDTO registerRequest = new UsuarioRequestDTO();
-        registerRequest.setApellidoPaterno("Alvarado");
-        registerRequest.setApellidoMaterno("Sanchez");
+        // UsuarioRequestDTO registerRequest = new UsuarioRequestDTO();
+        // registerRequest.setApellidoPaterno("Alvarado");
+        // registerRequest.setApellidoMaterno("Sanchez");
         // registerRequest.setCorreo("luis@gmail.com");
-        registerRequest.setDni("12345678");
-        registerRequest.setNombre("Luis");
-        registerRequest.setUsername("luis12");
-        registerRequest.setPassword("12345");
-        registerRequest.setEstado(true);
+        // registerRequest.setDni("12345678");
+        // registerRequest.setNombre("Luis");
+        // registerRequest.setUsername("luis12");
+        // registerRequest.setPassword("12345");
+        // registerRequest.setEstado(true);
         
 
-        registerRequest.setRol(rolService.encontraRolById(1L));
+        // registerRequest.setRol(rolService.encontraRolById(1L));
 
-        authService.register(registerRequest);
+        // authService.register(registerRequest);
 
         // PRODUCTOS
-        ProductoDTO producto = new ProductoDTO();
-        producto.setAlmacen(almacenService.encontrarAlmacenById(1L));
-        producto.setCantidad(30);
-        producto.setCategoria(categoriaService.encontrarCategoriaById(1L));
-        producto.setDescripcion("Lavadora M10");
-        producto.setEstadoProducto(true);
-        producto.setPeso("40kg");
-        producto.setSerie("series 1000");
-        producto.setUbicacion("zona 2, pasillo 1");
-        
-        ProductoDTO producto2 = new ProductoDTO();
-        producto2.setAlmacen(almacenService.encontrarAlmacenById(1L));
-        producto2.setCantidad(50);
-        producto2.setCategoria(categoriaService.encontrarCategoriaById(1L));
-        producto2.setDescripcion("Televisor LCD");
-        producto2.setEstadoProducto(true);
-        producto2.setPeso("5kg");
+        // ProductoDTO producto = new ProductoDTO();
+        // producto.setAlmacen(almacenService.encontrarAlmacenById(1L));
+        // producto.setCantidad(30);
+        // producto.setCategoria(categoriaService.encontrarCategoriaById(1L));
+        // producto.setDescripcion("Lavadora M10");
+        // producto.setEstadoProducto(true);
+        // producto.setPeso("40kg");
         // producto.setSerie("series 1000");
-        producto2.setUbicacion("zona 3, pasillo 1");
+        // producto.setUbicacion("zona 2, pasillo 1");
+        
+        // ProductoDTO producto2 = new ProductoDTO();
+        // producto2.setAlmacen(almacenService.encontrarAlmacenById(1L));
+        // producto2.setCantidad(50);
+        // producto2.setCategoria(categoriaService.encontrarCategoriaById(1L));
+        // producto2.setDescripcion("Televisor LCD");
+        // producto2.setEstadoProducto(true);
+        // producto2.setPeso("5kg");
+        // producto.setSerie("series 1000");
+        // producto2.setUbicacion("zona 3, pasillo 1");
 
-        List<ProductoDTO> productos = new ArrayList<>();
-        productos.add(producto);
-        productos.add(producto2);
+        // List<ProductoDTO> productos = new ArrayList<>();
+        // productos.add(producto);
+        // productos.add(producto2);
 
         // ORDEN ENTRADA
-        OrdenEntradaDTO ordenEntradaDTO = new OrdenEntradaDTO();
-        ordenEntradaDTO.setProductos(productoService.crearProductos(productos));
-        ordenEntradaDTO.setProveedor(proveedorService.encontrarProveedorById(1L));
-        ordenEntradaDTO.setPuntoLlegada("Av. Duarez #123");
-        ordenEntradaDTO.setUsuario(usuarioService.encontrarUsuarioById(1L));
+        // OrdenEntradaDTO ordenEntradaDTO = new OrdenEntradaDTO();
+        // ordenEntradaDTO.setProductos(productoService.crearProductos(productos));
+        // ordenEntradaDTO.setProveedor(proveedorService.encontrarProveedorById(1L));
+        // ordenEntradaDTO.setPuntoLlegada("Av. Duarez #123");
+        // ordenEntradaDTO.setUsuario(usuarioService.encontrarUsuarioById(1L));
         
-        ordenEntradaService.crearOrdenEntrada(ordenEntradaDTO);
+        // ordenEntradaService.crearOrdenEntrada(ordenEntradaDTO);
 
 
         // ORDEN SALIDA
-        OrdenSalidaDTO ordenSalidaDTO = new OrdenSalidaDTO();
-        ordenSalidaDTO.setDestino("Av. Abancay #12");
-        ordenSalidaDTO.setUsuario(usuarioService.encontrarUsuarioById(1L));
+        // OrdenSalidaDTO ordenSalidaDTO = new OrdenSalidaDTO();
+        // ordenSalidaDTO.setDestino("Av. Abancay #12");
+        // ordenSalidaDTO.setUsuario(usuarioService.encontrarUsuarioById(1L));
         // ordenSalidaDTO.setObservacion("");
 
         // productosArray.add(productoService.encontrarProductoById(1L));
         
-        List<ProductoDTO> productosArray = new ArrayList<>();
+        // List<ProductoDTO> productosArray = new ArrayList<>();
 
-        Producto p = productoService.encontrarProductoById(1L);
-        ProductoDTO productoDTO1 = new ProductoDTO();
-                productoDTO1.setId(1L);
-                productoDTO1.setDescripcion(p.getDescripcion());
-                productoDTO1.setCantidad(p.getCantidad());
-                productoDTO1.setCategoria(p.getCategoria());
-                productoDTO1.setAlmacen(p.getAlmacen());
-                productoDTO1.setUltimaCantidadRetirada(5);
-                productoDTO1.setEstado(true);
-                productoDTO1.setEstadoProducto(p.getEstadoProducto());
-                productoDTO1.setPeso(p.getPeso());
-                productoDTO1.setSerie(p.getSerie());
-                productoDTO1.setUbicacion(p.getUbicacion());
-        productosArray.add(productoDTO1);
-
-        ordenSalidaDTO.setProductos(productosArray);
+        // Producto p = productoService.encontrarProductoById(1L);
+        // ProductoDTO productoDTO1 = new ProductoDTO();
+        //         productoDTO1.setId(1L);
+        //         productoDTO1.setDescripcion(p.getDescripcion());
+        //         productoDTO1.setCantidad(p.getCantidad());
+        //         productoDTO1.setCategoria(p.getCategoria());
+        //         productoDTO1.setAlmacen(p.getAlmacen());
+        //         productoDTO1.setUltimaCantidadRetirada(5);
+        //         productoDTO1.setEstado(true);
+        //         productoDTO1.setEstadoProducto(p.getEstadoProducto());
+        //         productoDTO1.setPeso(p.getPeso());
+        //         productoDTO1.setSerie(p.getSerie());
+        //         productoDTO1.setUbicacion(p.getUbicacion());
+        // productosArray.add(productoDTO1);
 
         // ordenSalidaDTO.setProductos(productosArray);
 
-        ordenSalidaService.crearOrdenSalida(ordenSalidaDTO);
+        // ordenSalidaDTO.setProductos(productosArray);
+
+        // ordenSalidaService.crearOrdenSalida(ordenSalidaDTO);
     }
     
     
